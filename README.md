d4216eb8960b59ce9e01b1a4aaa0bce8eb2e2e58
nhonuit <nguyennhon2192@gmail.com>
2023-07-10 16:36:57 +0700

    add some mouse and keyboard options

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Necessary info:
-1. Creating .env file and adding these following fields to run script:
-REPEAT_TIME=
-TYPING_STRING=
-MOUSE_X_POSITION=
-MOUSE_Y_POSITION=
+1. Creating .env file and adding these following fields to run script (you can custom all of these params):
+REPEAT_TIME=5000
+TYPING_STRING=Hello World
+MOUSE_X_POSITION=50
+MOUSE_Y_POSITION=0
+KEY_TAP=down
+NEED_MOVE_MOUSE=true
+NEED_SCROLL_MOUSE=true
+NEED_CLICK_MOUSE=true
+NEED_TYPING=true
 2. Installing node and running "npm install" to install all necessary packages
 # How to run this script?
 1. Using node execution:
--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const robot = require('robotjs')
-require('dotenv').config()
+const robot = require("robotjs");
+require("dotenv").config();
 
 const work = () => {
-    setInterval(()=> {
-        //typing
-        robot.typeString(process.env.TYPING_STRING);
-        robot.keyTap("enter");
-        //mouse scroll
-        robot.scrollMouse(process.env.MOUSE_X_POSITION, process.env.MOUSE_Y_POSITION);
-        //move mouse
-        // robot.moveMouse(0, 0);
-        // robot.mouseToggle("down");
-        // robot.dragMouse(100, 100);
-        // robot.mouseToggle("up");
-        //mouse click
-        robot.mouseClick();
-    }, process.env.REPEAT_TIME)  
-    
-}
-work()
-// Type "Hello World".
+  setInterval(() => {
+    //typing
+    if (process.env.NEED_TYPING) {
+      robot.typeString(process.env.TYPING_STRING);
+      robot.keyTap("enter");
+      robot.keyTap(process.env.KEY_TAP);
+    }
+    //mouse scroll
+    if (process.env.NEED_SCROLL_MOUSE) {
+      robot.scrollMouse(
+        process.env.MOUSE_X_POSITION,
+        process.env.MOUSE_Y_POSITION
+      );
+    }
+    //move mouse
+    if (process.env.NEED_MOVE_MOUSE) {
+      robot.moveMouse(0, 0);
+      robot.mouseToggle("down");
+      robot.dragMouse(100, 100);
+      robot.mouseToggle("up");
+    }
+    if (process.env.NEED_CLICK_MOUSE) {
+      robot.mouseClick();
+    }
+    //mouse click
+  }, process.env.REPEAT_TIME);
+};
+work();
